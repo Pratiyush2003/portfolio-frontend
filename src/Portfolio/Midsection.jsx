17f516679d4aa3afba54390ff0faffa14a2494f9
pratiyush singh <pratiyush12@outlook.in>
2024-07-09 16:10:57 +0530

    Add existing project files prior to the push to GitHub.

--- src/Portfolio/Midsection.jsx
+++ src/Portfolio/Midsection.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { ReactTyped } from "react-typed";
-import portfolioimg from "../assets/portfolio5.jpg"
+import portfolioimg from "../assets/portfolio4.jpg"
 
 const Midsection = () => {
     return (
         <div className='grid place-content-center '>
             <div className='grid md:grid-cols-2 mt-18 md:mt-20 place-items-center'>
             <div className='m-6'>
-                <img src={portfolioimg} className='rounded-full h-80 md:h-96' />
+                <img src={portfolioimg} className='rounded-full h-80 w-80' />
             </div>
             <div className=' text-center m-6'>
                 <p className='text-gray-600 font-bold'>Hello, I'm</p>
                 <h1 className='md:text-6xl text-4xl font-bold'>Pratiyush</h1>
-                <h1 className='md:text-4xl text-3xl text-gray-400 text-center ml-4 mb-4'>
+                <h1 className='md:text-4xl text-3xl text-gray-400 ml-12 mb-4'>
                     <ReactTyped 
                         strings={[
                             "Full-Stack Mern Developer",
--- src/Portfolio/Projects.jsx
+++ src/Portfolio/Projects.jsx
@@ -4,9 +4,11 @@ import Mycontext from "../context/Mycontext";
 const Projects = () => {
   const context = useContext(Mycontext);
   const { fetchProjects, allProjects } = context;
+
   useEffect(() => {
     fetchProjects();
   }, []);
+
   return (
     <>
       <h1 className="md:text-6xl text-4xl font-bold text-center mt-4" id="js_Project">
@@ -14,29 +16,27 @@ const Projects = () => {
       </h1>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 m-10 md:m-20 mb-36 justify-items-center">
-        {allProjects.map((p , index) => {
-          return (
-            <div className=" bg-contain bg-center shadow-lg p-2 rounded-lg" key={index}>
-              <div className="overflow-hidden rounded-lg">
-                <img
-                  src={p.image}
-                  alt=""
-                  className="h-60 w-96 hover:scale-125 duration-1000"
-                />
-              </div>
-              <p className="text-center text-2xl font-bold m-6">{p.projectname}</p>
-              <div className="flex items-center justify-around">
-                <button className="px-6 py-2 rounded-full text-[#2d2e2e] hover:text-white hover:bg-gray-500 border-2 border-rounded-900 m-2 font-bold">
-                  <a href={p.github}>GITHUB</a>
-                </button>
-
-                <button className="px-6 py-2 rounded-full text-[#2d2e2e] hover:text-white hover:bg-gray-500 border-2 border-rounded-900 m-2 font-bold">
-                  <a href={p.hostlink}>WEBSITE</a>
-                </button> 
-              </div>
+        {allProjects.map((p, index) => (
+          <div className="bg-contain bg-center shadow-lg p-2 rounded-lg" key={index}>
+            <div className="overflow-hidden rounded-lg">
+              <img
+                src={p.image}
+                alt=""
+                className="h-60 w-96 hover:scale-125 duration-1000"
+              />
             </div>
-          );
-        })}
+            <p className="text-center text-2xl font-bold m-6">{p.projectname}</p>
+            <div className="flex items-center justify-around">
+              <button className="px-6 py-2 rounded-full text-[#2d2e2e] hover:text-white hover:bg-gray-500 border-2 border-rounded-900 m-2 font-bold">
+                <a href={p.github} target="_blank" rel="noopener noreferrer">GITHUB</a>
+              </button>
+
+              <button className="px-6 py-2 rounded-full text-[#2d2e2e] hover:text-white hover:bg-gray-500 border-2 border-rounded-900 m-2 font-bold">
+                <a href={p.hostlink} target="_blank" rel="noopener noreferrer">WEBSITE</a>
+              </button> 
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
